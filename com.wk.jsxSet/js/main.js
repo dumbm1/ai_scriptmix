a948663595a1f3e410beb35f444c0773015e4fca
Marat Shagiev <m_js@bk.ru>
2016-09-03 00:52:09 +0300

    init project from version 1.1

---
+++ com.wk.jsxSet/js/main.js
@@ -0,0 +1,117 @@
+/**
+ * jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50
+ * */
+/**
+ * global $, window, location, CSInterface, SystemPath, themeManager
+ * */
+
+(function () {
+  'use strict';
+
+  var csInterface = new CSInterface ();
+  init ();
+
+  function init () {
+
+    themeManager.init ();
+
+    loadJSX ("json2.js");
+
+    var userData  = csInterface.getSystemPath (SystemPath.USER_DATA),
+        store     = userData + '/LocalStore/jsxSet/',
+        storeCfg  = store + 'cfg/',
+        storeImg  = store + 'img/',
+        storeJsx  = store + 'jsx/',
+        btnsList  = storeCfg + 'btns_List.txt',
+        btnsNames = [],
+
+        divBtns   = document.getElementById('btns'),
+        i;
+
+    // load the buttons names from file
+    csInterface.evalScript ('readlnBtnsList()', function (result) {
+      if (result.length == 0) return;
+      btnsNames = JSON.parse(result);
+
+      for (i = 0; i < btnsNames.length; i++) {
+        addBtnToInterface (btnsNames[i]);
+      }
+    });
+
+    /**
+     * SERVICE BUTTONS HANDLERS
+     * */
+    $ ("#btn_addBtn").click (function () {
+      csInterface.evalScript ('addBtnToStore("' + store + '" )',
+        function (result) {
+          if (result.length != 0) {
+            addBtnToInterface (result);
+          }
+        });
+    });
+    $ ("#btn_prefs").click (function () {
+      csInterface.evalScript ('changePrefs("' + storeCfg + '" )',
+        function (result) {
+          if (result.length == 0) return;
+          reloadPanel ();
+        });
+    });
+    $ ("#btn_killCEP").click (function () {
+      csInterface.evalScript ('killCEP()');
+    });
+    $ ("#btn_reload").click (function () {
+      reloadPanel ();
+    });
+    $ ("#btn_source").click (function () {
+      csInterface.evalScript ('openFolder("' + store + '")');
+    });
+
+    /** * * * * * * * * * * * *
+     * * * jsxSet library * * *
+     * * * * * * * * * * * * **/
+
+    function addBtnToInterface (btnName) {
+      if (btnName.length == 0) return;
+      var btn = document.createElement('input');
+      btn.setAttribute('type', 'image');
+      btn.setAttribute('class', 'btn');
+      btn.setAttribute('src', storeImg + btnName + '.png');
+      btn.setAttribute('title', btnName);
+      btn.setAttribute('name', btnName);
+      btn.setAttribute('id', 'btn_' + btnName);
+
+      btn.addEventListener('click', function () {
+        csInterface.evalScript ('$.evalFile("' + storeJsx + btnName + '.jsx' + '")');
+      })
+
+      btn.oncontextmenu = function () {
+        var delName    = this.name,
+            btnToDel   = document.getElementById('btn_' + btnName),
+            confirmDel = confirm ('Delete script: ' + delName + '?');
+        if (confirmDel) {
+          csInterface.evalScript ('delBtnFromStore("' + delName + '" )', function (result) {
+            btnToDel.remove();
+          });
+        }
+        return false;
+      }
+
+      divBtns.appendChild(btn);
+    }
+
+  }
+
+  /** * * * * * * * * * *
+   * * * NATIVE LIB * * *
+   * * * * * * * * * * **/
+  // Reloads extension panel
+  function reloadPanel () {
+    location.reload ();
+  }
+
+  function loadJSX (fileName) {
+    var extensionRoot = csInterface.getSystemPath (SystemPath.EXTENSION) + "/jsx/";
+    csInterface.evalScript ('$.evalFile("' + extensionRoot + fileName + '")');
+  }
+} ());
+    
---
+++ com.wk.jsxSet/jsx/hostscript.jsx
@@ -0,0 +1,255 @@
+//@target illustrator
+var storePath = Folder.userData + '/LocalStore/jsxSet/',
+    store     = new Folder (storePath),
+    storeCfg  = new Folder (storePath + '/cfg/'),
+    storeImg  = new Folder (storePath + '/img/'),
+    storeJsx  = new Folder (storePath + '/jsx/'),
+    btnsList  = storeCfg + '/btns_List.txt';
+
+if (!store.exists) store.create();
+if (!storeCfg.exists) storeCfg.create();
+if (!storeImg.exists) storeImg.create();
+if (!storeJsx.exists) storeJsx.create();
+
+function addBtnToStore () {
+
+  var resultName = '',
+      sourseJsx, targJsx,
+      soursePng, targPng;
+
+  var w          = new Window ('dialog', 'Add script button'),
+      pn         = w.add('panel'),
+      gr_addJsx  = pn.add('group'),
+      gr_addPng  = pn.add('group'),
+      gr_stdBtns = w.add('group'),
+
+      btn_addJsx = gr_addJsx.add('button', undefined, 'Add jsx-script'),
+      txt_addJsx = gr_addJsx.add('edittext'),
+
+      btn_addPng = gr_addPng.add('button', undefined, 'Add png-image'),
+      txt_addPng = gr_addPng.add('edittext'),
+
+      btn_ok     = gr_stdBtns.add('button', undefined, 'Ok'),
+      btn_cancel = gr_stdBtns.add('button', undefined, 'Cancel');
+
+  txt_addJsx.characters = txt_addPng.characters = 30;
+  btn_addPng.preferredSize.width = btn_addJsx.preferredSize.width = 110;
+
+  btn_addPng.onClick = function () {
+    soursePng       = File.openDialog ('Select a png-image file', 'PNG files:*.png', false);
+    txt_addPng.text = soursePng.fsName;
+  }
+
+  btn_addJsx.onClick = function () {
+    sourseJsx       = File.openDialog ('Select a jsx-script file', 'JSX files:*.jsx', false);
+    txt_addJsx.text = sourseJsx.fsName;
+  }
+
+  btn_ok.onClick = function () {
+    resultName = (sourseJsx.name).slice(0, -4);
+
+    sourseJsx.copy(store + '/jsx/' + resultName + '_.jsx');
+    targJsx = new File (store + '/jsx/' + resultName + '_.jsx');
+    try {
+      (new File (store + '/jsx/' + resultName + '.jsx')).remove();
+    } catch (e) {
+    }
+    targJsx.rename(resultName + '.jsx');
+
+    soursePng.copy(store + '/img/' + resultName + '_.png');
+    targPng = new File (store + '/img/' + resultName + '_.png');
+    try {
+      (new File (store + '/img/' + resultName + '.png')).remove();
+    } catch (e) {
+    }
+    targPng.rename(resultName + '.png');
+
+    var btnsNames = [];
+    var f         = new File (btnsList),
+        btnsNames = [];
+
+    f.open('r');
+    while (!f.eof) {
+      btnsNames.push(f.readln());
+    }
+    f.close();
+
+    btnsNames.push(resultName);
+
+    f.open('w');
+    for (var i = 0; i < btnsNames.length; i++) {
+      f.writeln(btnsNames[i]);
+    }
+    f.close();
+
+    w.close();
+  }
+
+  w.show();
+
+  return resultName;
+}
+
+function delBtnFromStore (delName) {
+
+  var f         = new File (btnsList),
+      btnsNames = [],
+      i;
+
+  (new File (storeImg + '/' + delName + '.png')).remove();
+  (new File (storeJsx + '/' + delName + '.jsx')).remove();
+
+  f.open('r');
+  while (!f.eof) {
+    btnsNames.push(f.readln());
+  }
+  f.close();
+
+  for (var i = 0; i < btnsNames.length; i++) {
+    if (btnsNames[i] == delName) {
+      btnsNames.splice(i, 1);
+      break;
+    }
+  }
+
+  f.open('w');
+  for (var i = 0; i < btnsNames.length; i++) {
+    f.writeln(btnsNames[i]);
+  }
+  f.close();
+}
+
+/**
+ * THE FILE OPERATIONS FOR MAIN.JS
+ * */
+function readlnBtnsList () {
+
+  var result = [],
+      f      = new File (btnsList);
+
+  if (!f.exists) return result;
+
+  f.open('r');
+
+  while (!f.eof) {
+    result.push(f.readln());
+  }
+  f.close();
+
+  //return result;
+  return JSON.stringify(result);
+}
+
+function writelnBtnsList (arr) {
+
+  arr   = arr || [];
+  var f = new File (btnsList);
+  f.open('w');
+  for (var i = 0; i < arr.length; i++) {
+    f.writeln(arr[i]);
+  }
+  f.close();
+}
+
+function openFolder (path) {
+  var folder = new Folder (path);
+  folder.execute();
+}
+
+/**
+ * OTHER
+ * */
+
+function killCEP () {
+  /**
+   * make bat-file that kill all system processes CEPHTMLEngine.exe
+   */
+  _execFile (
+    Folder.temp.absoluteURI + '/' + 'tasks_kill.bat',
+    'taskkill /IM CEPHTMLEngine.exe /f'
+  );
+  /**
+   * make new file by full path, write to disk with some file contenr, execute file
+   *
+   * @param {String} filePath - FULL path (include file-extension)
+   * @param {String} fileContent - content to new file
+   */
+  function _execFile (filePath, fileContent) {
+    var f = new File (filePath);
+    f.open ('e');
+    f.write (fileContent);
+    f.close ();
+    f.execute ();
+  }
+}
+
+function changePrefs () {
+
+  var btnsNames = [],
+      f         = new File (btnsList);
+  f.open('r');
+  while (!f.eof) {
+    btnsNames.push(f.readln());
+  }
+  f.close();
+
+  var w          = new Window ("dialog", "Rearrange buttons"),
+      list       = w.add ("listbox", undefined, btnsNames, {multiselect: true}),
+      grUpDown   = w.add('group'),
+      up         = grUpDown.add ("button", undefined, "Up"),
+      down       = grUpDown.add ("button", undefined, "Down"),
+      grOkCancel = w.add('group'),
+      ok         = grOkCancel.add('button', undefined, 'OK'),
+      cancel     = grOkCancel.add('button', undefined, 'Cancel');
+
+  ok.onClick = function () {
+    var arr = [];
+
+    for (var i = 0; i < list.items.length; i++) {
+      arr.push(list.items[i]);
+    }
+
+    var f = new File (btnsList);
+    f.open('w');
+    for (var i = 0; i < arr.length; i++) {
+      f.writeln(arr[i]);
+    }
+    f.close();
+
+    w.close();
+  }
+
+  up.onClick = function () {
+    var first = list.selection[0].index;
+    if (first == 0 || !contiguous (list.selection)) return;
+    var last = first + list.selection.length;
+    for (var i = first; i < last; i++)
+      swap (list.items [i - 1], list.items [i]);
+    list.selection = null;
+    for (var i = first - 1; i < last - 1; i++)
+      list.selection = i;
+  }
+
+  down.onClick = function () {
+    var last = list.selection.pop().index;
+    if (last == list.items.length - 1 || !contiguous (list.selection)) return;
+    var first = list.selection[0].index;
+    for (var i = last; i >= first; i--)
+      swap (list.items [i + 1], list.items [i]);
+    list.selection = null;
+    for (var i = first + 1; i <= last + 1; i++)
+      list.selection = i;
+  }
+
+  function contiguous (sel) {
+    return sel.length == (sel[sel.length - 1].index - sel[0].index + 1);
+  }
+
+  function swap (x, y) {
+    var temp = x.text;
+    x.text   = y.text;
+    y.text   = temp;
+  }
+
+  w.show ();
+}
